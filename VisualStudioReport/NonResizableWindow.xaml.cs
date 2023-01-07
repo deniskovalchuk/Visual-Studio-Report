7bd368cc29744108f2331d9469f95d30f2893c90
Denis Kovalchuk <Denis.Kovalchuk@ostyserver.net>
2023-01-08 02:10:32 +0300

    Add a project with a reproduction.

---
+++ VisualStudioReport/NonResizableWindow.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.VisualStudio.PlatformUI;
+
+namespace VisualStudioReport
+{
+    public partial class NonResizableWindow : DialogWindow
+    {
+        public NonResizableWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
